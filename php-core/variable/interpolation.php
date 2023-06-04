0c7f661ab0bb3fea812cda774b0a01088698dc86
ITSBHM Group - Mars <mars@itsbhm.group>
2023-06-04 23:00:49 +0530

    interpolation File Added

---
+++ php-core/variable/interpolation.php
@@ -0,0 +1,14 @@
+<?php
+
+$greet = "Hello";
+$lang = 'PHP';
+
+echo "{$greet}, Im {$lang}";
+
+echo '<hr>';
+
+echo "{$greet}, Im '{$lang}'";
+
+echo '<hr>';
+
+echo $greet ." Im ". "'" . $lang. "'";
