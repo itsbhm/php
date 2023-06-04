b3dbba7e6825c704fbb1f3350ffb99b1ad9e3bcd
ITSBHM Group - Mars <mars@itsbhm.group>
2023-06-04 12:57:24 +0530

    Variable Module Added

---
+++ php-core/variable/index.php
@@ -0,0 +1,15 @@
+<?php
+
+$name = "John"; // Declaring and assigning a string value to the $name variable
+$age = 25; // Declaring and assigning a numeric value to the $age variable
+$isActive = true; // Declaring and assigning a boolean value to the $isActive variable
+
+
+$name = "John"; // Assigning an initial value to $name variable
+$name = "Mike"; // Changing the value of $name variable
+
+$age = 25; // Assigning an initial value to $age variable
+$age = $age + 1; // Modifying the value of $age variable by adding 1
+
+$isActive = true; // Assigning an initial value to $isActive variable
+$isActive = !$isActive; // Negating the value of $isActive variable
